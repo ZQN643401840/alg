956f63798f37fc6066ea647e396e9bb706a81bd3
zhouqinan <643401840@qq.com>
2023-01-19 22:12:19 +0800

    add 15

--- 15桶排序/main.cpp
+++ 15桶排序/main.cpp
@@ -151,42 +151,71 @@ private:
 
 };  
 
+// 计算一个数有几个位,个位,十位,千位
+
+
+// 拼音命名函数,桶排序
+void BucketSort(int* data, uint64_t size)
+{
+  // 声明,加定义桶,相同的桶可能有多个数,所以用内层的 std::vector<int> 来存储 
+  std::vector<std::vector<int>> vec2_int;  
+
+  // 有数它仍然有位  
+  auto mod{10};  
+  auto dev{1};
+  auto flag_should_be_dev{false};
+  do
+  {
+    vec2_int.resize(10);
+    flag_should_be_dev = false;
+    // 遍历这些数,作桶排序
+    for (auto i = 0; i < size; i++)
+    {
+      auto bucket_num = (data[i] / dev) % mod;  
+      // 桶的序号不为0,说明仍然可以来作为被除数来除  
+      flag_should_be_dev = (0 != bucket_num);
+      // 根据这个数所属的桶的序号将它放到对应的桶内      
+      // 桶认为是小数在前 0 - 9,这样的话就是从小到大排序  
+      vec2_int[bucket_num].emplace_back(data[i]);  
+      // 从大到小排序
+      // vec2_int[9 - bucket_num].emplace_back(data[i]);  
+    }
+
+    // 二维数组,但是我知道它们其实是线性分布的,前提是将内层的 vector 要 shrink_to_fit();这样内层的 vector 才是连续的
+    for (auto& each_vec : vec2_int) each_vec.shrink_to_fit();  
+
+    memcpy(data, &(vec2_int[0][0]), size * sizeof(int));
+    // mod*=10;
+    dev*=10;
+  } while (flag_should_be_dev);  
+}
+
 int main()
 {
-  // 大根堆就用这个
-  Cmp greater = [](int a, int b){ return a > b;};      
-  
-  // 小根堆用这个函数对象
-  Cmp less = [](int a, int b){ return a < b;};    
-
-  // 大根堆
-  // PriorityQueue queue(less);    
-  // 小根堆
-  PriorityQueue queue(greater);    
 
   // 大根堆  
   // std::priority_queue<int, std::vector<int>, std::less<int>> std_queue;
   // 小根堆
   std::priority_queue<int, std::vector<int>, std::greater<int>> std_queue;  
 
+  std::vector<int> vec_int;
   srand(time(0));
   for (uint64_t i = 0; i < 1000; i++)
   {
     auto rand_num = rand() % 1000;
-    queue.Push(rand_num);   
-    std_queue.push(rand_num); 
+    std_queue.push(rand_num);   
+    vec_int.emplace_back(rand_num);
   }
-  
+
+  BucketSort(&vec_int[0], vec_int.size());  
+
   auto cnts{0};
-  while (!queue.Empty() || !std_queue.empty())
+  while (!std_queue.empty())
   {
     auto flag{false};  
-    std::cout << "top num: " << queue.Top() << std::endl;
-    flag = (queue.Top() == std_queue.top());
+    flag = (vec_int[cnts] == std_queue.top());
     if (!flag) std::cout << "cnts: " << cnts << std::endl;  
     assert(flag);
-    
-    queue.Pop();  
     std_queue.pop();    
     cnts++;
   }
